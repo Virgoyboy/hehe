7b97a653402f975e18a9c4e220ecab7b60d34991
Huy Vuong <157224829+Virgoyboy@users.noreply.github.com>
2024-01-21 09:10:06 +0700

    Update Tree_array (1).cpp

--- Tree_array (1).cpp
+++ Tree_array (1).cpp
@@ -1,42 +1,6 @@
-#include "iostream"
-using namespace std;
-const int N = 100;
-//Luu cac con
-struct Node{
-	int Id;
-	Node *next;
-};
+#include <iostream>
 
-struct Member{
-	int infor;
-	Node *child;
-};
-
-struct Tree{
-	Member elems[N];
-	int size;
-};
-int eldestChild(int k, Tree T){
-	if(T.elems[k].child != NULL)
-		T.elems[k].child->Id;
-	else
-		return -1;
-}
-int parent(int k, Tree T){
-	int i = 0;
-	Node *p;
-	while(i < T.size){
-		p = T.elems[i].child;
-		while(p != null)
-			if(p->Id == k)
-				return i;
-			else 
-				p = p->next;		
-		i++;
-	}
-	
-	return -1;
-}
-int main(){
-	
+int main() {
+    std::cout << "Hello World!";
+    return 0;
 }

68bf5613bdf4e6793966af47aef0516016c3188e
dHung2412 <148836286+dHung2412@users.noreply.github.com>
2024-01-21 09:46:40 +0700

    Update Tree_array (1).cpp

--- Tree_array (1).cpp
+++ Tree_array (1).cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hello World!" << endl;
+    cout << "Anh em tao ma cao" << endl;
     cout << "Chào mừng đến với nhóm 1"; 
     return 0;
 }
